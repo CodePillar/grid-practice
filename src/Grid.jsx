f98d35a767c8ada0b7c45db7639118040bb7e952
CodePillar <naruhodo.dev@gmail.com>
2024-05-25 00:24:57 +0700

    first-commit

---
+++ src/Grid.jsx
@@ -0,0 +1,15 @@
+function Grid() {
+  return (
+    <>
+      <div className="header-bar">
+        <div className="minibox1">Item1</div>
+        <div className="minibox2">Item2</div>
+        <div className="minibox3">Item3</div>
+        <div className="minibox4">Item4</div>
+        <div className="minibox5">Item5</div>
+      </div>
+    </>
+  );
+}
+
+export default Grid;
